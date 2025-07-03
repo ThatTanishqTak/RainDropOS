d7ceb076384345ad864a3041afb491fcdd54f748
ThatTanishqTak <tanishqtak3@gmail.com>
2025-07-04 00:27:37 +0100

    Expanded the bootstrap

--- asm/bootstrap.asm
+++ asm/bootstrap.asm
@@ -1,7 +1,19 @@
-; A minimal NASM stub
+; asm/bootstrap.asm
+bits 16
+org 0x7C00
 global _start
 
-section .text
 _start:
-    ; Placeholder boot code
-    ret
+    cli                 ; disable interrupts
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00      ; stack grows down from the bootloader location
+
+    ; Here we could load further sectors or jump directly to C++ code
+    call Boot           ; Boot is our C++17 entry function
+    jmp $               ; hang after Boot returns
+
+times 510 - ($-$$) db 0 ; pad out to 512 bytes
+dw 0xAA55              ; boot signature
